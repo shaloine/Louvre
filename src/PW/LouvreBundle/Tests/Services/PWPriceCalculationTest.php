ea35258c33611a9b93e2f10373fc2d9accd6a264
shaloine <shaloine2@hotmail.com>
2017-07-01 06:16:09 +0200

    test unitaire

---
+++ src/PW/LouvreBundle/Tests/Services/PWPriceCalculationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PW\LouvreBundle\Tests\Services;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use PW\LouvreBundle\Entity\Reservation;
+use PW\LouvreBundle\Entity\Visitor;
+use \DateTime;
+
+
+class PWPriceCalculationTest extends WebTestCase
+{
+
+    public function testsetPrice()
+    {
+    	$reservation = new Reservation();
+    	$reservation->setDemi(1);
+
+    	$visitor1 = new Visitor();
+    	$visitor1->setDateNaissance(new \DateTime('1985/01/01'));
+    	$visitor1->setReduit(1);
+
+    	$reservation->addVisitor($visitor1);
+    	
+
+    	//chargement du service
+        $kernel = static::createKernel();
+ 		$kernel->boot();
+ 		$container = $kernel->getContainer();
+ 		$PriceCalculation = $container->get('pw_louvre.priceCalculation');
+
+
+ 		$PriceCalculation->setPrice($reservation);
+
+ 		$resultat = $reservation->getPrixTotal();
+
+ 		$this->assertEquals(10, $resultat);
+    }
+}
