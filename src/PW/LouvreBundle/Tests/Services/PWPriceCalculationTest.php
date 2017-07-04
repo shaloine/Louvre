3ef00a298c813c165e16a7ee58c4ca508f0fd769
Nicolas Micallef <nicolas.micallef@biboard.fr>
2017-07-04 17:21:24 +0200

    test unit

--- src/PW/LouvreBundle/Tests/Services/PWPriceCalculationTest.php
+++ src/PW/LouvreBundle/Tests/Services/PWPriceCalculationTest.php
@@ -11,7 +11,7 @@ use \DateTime;
 class PWPriceCalculationTest extends WebTestCase
 {
 
-    public function testsetPrice()
+    public function testOneAdult()
     {
     	$reservation = new Reservation();
     	$reservation->setDemi(1);
@@ -35,5 +35,31 @@ class PWPriceCalculationTest extends WebTestCase
  		$resultat = $reservation->getPrixTotal();
 
  		$this->assertEquals(10, $resultat);
+    }
+
+    public function testsetPrice2()
+    {
+        $reservation = new Reservation();
+        $reservation->setDemi(1);
+
+        $visitor1 = new Visitor();
+        $visitor1->setDateNaissance(new \DateTime('1985/01/01'));
+        $visitor1->setReduit(1);
+
+        $reservation->addVisitor($visitor1);
+        
+
+        //chargement du service
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $container = $kernel->getContainer();
+        $PriceCalculation = $container->get('pw_louvre.priceCalculation');
+
+
+        $PriceCalculation->setPrice($reservation);
+
+        $resultat = $reservation->getPrixTotal();
+
+        $this->assertEquals(10, $resultat);
     }
 }
